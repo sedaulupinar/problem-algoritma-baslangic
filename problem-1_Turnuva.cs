3d397fec5cdaf3523179d30d0e9114c2c91d186b
sedaulupinar <seda.ulupinarrrr@gmail.com>
2023-08-22 13:03:03 +0300

    sorubircozumu

--- problem-1_Turnuva.cs
+++ problem-1_Turnuva.cs
@@ -3,3 +3,5 @@ problem-1 //Turnuva
 /* 4 takımın katıldıgı bir turnuvada sampiyon takım en cok galibiyeti alan takım olacaktır. 
 her takım her takımla maç yapmıstır. bjk,ts,gs,fb takımlardır. 
 sonuclar BJK-gs; FB-ts; TS-gs; GS-fb; BJK-ts; BJK-fb. sampiyon takımı bulunuz. (if-else)*/
+
+Cozum....
