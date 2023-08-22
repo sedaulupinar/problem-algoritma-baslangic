6091f84ff02aaf153642b7328f63ad31c9245327
sedaulupinar <seda.ulupinarrrr@gmail.com>
2023-08-22 12:57:15 +0300

    .

--- problem-1_Turnuva.cs
+++ problem-1_Turnuva.cs
@@ -4,3 +4,4 @@ problem-1 //Turnuva
 her takım her takımla maç yapmıstır. bjk,ts,gs,fb takımlardır. 
 sonuclar BJK-gs; FB-ts; TS-gs; GS-fb; BJK-ts; BJK-fb. sampiyon takımı bulunuz. (if-else)*/
 
+deneme
